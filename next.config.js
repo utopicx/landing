db03245aeebb097dbc0d85fdbb037a399eea6e37
Ariel Santiago Villarreal Gutierrez <arielskap@gmail.com>
2022-08-23 15:10:52 -0500

    fix: new env

--- next.config.js
+++ next.config.js
@@ -2,7 +2,10 @@
 const nextConfig = {
   reactStrictMode: false,
   images: {
-    domains: [process.env.API_URL.replace(/(^\w+:|^)\/\//, "")],
+    domains: [
+      process.env.API_URL.replace(/(^\w+:|^)\/\//, ""),
+      process.env.NEXT_PUBLIC_IMAGE_URL,
+    ],
   },
 };
 module.exports = nextConfig;
