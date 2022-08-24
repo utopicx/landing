0c0bba4c8c3660c13bf6b5c82392ca65ab5c104a
Ariel Santiago Villarreal Gutierrez <arielskap@gmail.com>
2022-08-23 19:45:50 -0500

    fix seo

--- src/components/Seo.tsx
+++ src/components/Seo.tsx
@@ -6,13 +6,13 @@ const Seo: FC = () => {
   const { state } = useGlobal();
   return (
     <NextSeo
-      title={state.texts?.seo.Title || "Utópicx"}
-      description={state.texts?.seo.Description || "Utópicx landing page."}
-      canonical={state.texts?.seo.URL || "https://utopicx.com"}
+      title={state.texts?.seo?.Title || "Utópicx"}
+      description={state.texts?.seo?.Description || "Utópicx landing page."}
+      canonical={state.texts?.seo?.URL || "https://utopicx.com"}
       openGraph={{
-        url: state.texts?.seo.URL || "https://utopicx.com",
-        title: state.texts?.seo.Title || "Utópicx",
-        description: state.texts?.seo.Description || "Utópicx landing page.",
+        url: state.texts?.seo?.URL || "https://utopicx.com",
+        title: state.texts?.seo?.Title || "Utópicx",
+        description: state.texts?.seo?.Description || "Utópicx landing page.",
       }}
     />
   );
