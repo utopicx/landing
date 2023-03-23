85da03f698733968d5bd75d1cf0b2450b6a8c802
Ariel Santiago Villarreal Gutierrez <arielskap@gmail.com>
2023-03-23 16:44:29 -0300

    Feat: Major Seo

--- src/components/Seo.tsx
+++ src/components/Seo.tsx
@@ -4,9 +4,10 @@ import { useGlobal } from "../context/global/Context";
 
 const Seo: FC = () => {
   const { state } = useGlobal();
+  const titleSection = state.section ? `${state.section} | ` : "";
   return (
     <NextSeo
-      title={state.texts?.seo?.Title || "Utópicx"}
+      title={`${titleSection}${state.texts?.seo?.Title || "Utópicx"}`}
       description={state.texts?.seo?.Description || "Utópicx landing page."}
       canonical={state.texts?.seo?.URL || "https://utopicx.com"}
       openGraph={{
