ce7f88ffe3fe3b64223035e4c04a7fd9d295892e
Ariel Santiago Villarreal Gutierrez <arielskap@gmail.com>
2023-06-22 20:05:29 -0600

    Feat: privacy.tsx

---
+++ src/pages/privacy.tsx
@@ -0,0 +1,212 @@
+const Privacy = () => {
+  return (
+    <div>
+      <h3>AVISO DE PRIVACIDAD</h3>
+      <p>
+        Utopicx mantiene su compromiso de respetar su privacidad mientras usa
+        nuestro sitio web. La política a continuación es aplicable a las
+        prácticas de recopilación de información de visitantes de nuestro sitio
+      </p>
+      <h4>Definicíon de Términos</h4>
+      <p>
+        Cualquier información relacionada con una persona física se denomina
+        &quot;información de identificación personal&quot; (o &quot;PII&quot;).
+        De forma indirecta o directa, esta información, utilizada en combinación
+        con otra información disponible o probablemente disponible para una
+        entidad corporativa, es capaz y posiblemente puede identificar a dicha
+        persona.
+      </p>
+      <h4>Información Que Recopilamos:</h4>
+      <p>
+        A menos que elija completar y enviar su información en nuestro sitio web
+        a través de nuestros formularios proporcionados específicamente,
+        funciones interactivas de chat en vivo, u otros medios, Utopicx no
+        recopila información de identificación personal sobre usted. Con el fin
+        de proporcionar a nuestros visitantes una mejor experiencia y
+        usabilidad, recopilamos cierta información automáticamente. Cuando
+        visita nuestro sitio web, recopilamos el sistema operativo que usa, su
+        dirección IP y el navegador que está utilizando para proporcionar una
+        experiencia mejor y más personal.
+      </p>
+      <p>
+        Utopicx solo procesará la información que recibamos a fin de buscar
+        intereses comerciales legítimos para establecer comunicaciones con
+        posibles / actuales clientes y otras personas con consultas generales
+        relacionadas con el negocio.
+      </p>
+      <h4>Uso Normal de la Información Sobre Usted</h4>
+      <p>
+        Para mejorar nuestro sitio web, utilizamos cierta información para
+        analizar el uso de nuestro sitio.
+      </p>
+      <p>
+        Cualquier información de identificación personal que proporcione, la
+        usamos para responder a su consulta o procesar un formulario de
+        solicitud que haya completado en nuestro sitio web. También podemos
+        usarlo para responder a un proceso legal o según lo exija la ley en
+        respuesta a una citación, una agencia de aplicación de la ley, una orden
+        judicial, para tomar medidas contra cualquier amenaza potencial a la
+        seguridad física de una persona o cualquier actividad ilegal.
+      </p>
+      <h4>Nuestra Política de Intercambio de Información</h4>
+      <p>
+        Podemos compartir su información de identificación personal dentro de
+        Utopicx, agentes externos autorizados en cualquier parte del mundo, o
+        nuestros socios comerciales con fines de enriquecimiento de datos,
+        almacenamiento, procesamiento o para proporcionar servicios a una
+        transacción que se solicitó, después de garantizar que dichas entidades
+        son ellas mismas, vinculadas contractualmente por obligaciones de
+        privacidad de datos. En caso de que transfiramos información de
+        identificación personal fuera de la UE, es a países determinados por la
+        Comisión Europea, para tener un nivel adecuado de protección, o se ha
+        determinado que cuentan con las garantías adecuadas.
+      </p>
+      <h4>Sus Derechos de Privacidad</h4>
+      <p>
+        Los visitantes de nuestro sitio pueden tener ciertos derechos
+        relacionados con su información de identificación personal que se
+        proporcionan según la ley aplicable. Estos derechos son:
+      </p>
+      <ul>
+        <li>
+          El derecho de solicitar acceso a su información de identificación
+          personal y actividades de procesamiento relacionadas.
+        </li>
+        <li>
+          El derecho a solicitar que se corrija cualquier información de
+          identificación personal inexacta o incompleta.
+        </li>
+        <li>
+          El derecho a solicitar que su información de identificación personal
+          sea borrada bajo ciertas circunstancias.
+        </li>
+        <li>
+          El derecho a solicitar que su procesamiento de información de
+          identificación personal sea restringido bajo ciertas circunstancias.
+        </li>
+        <li>
+          El derecho a oponerse a que se procese su Información de
+          identificación personal en determinadas circunstancias.
+        </li>
+        <li>
+          El derecho a recibir de nosotros, un controlador,4 su Información de
+          identificación personal proporcionada en un formato legible por
+          máquina, de uso común en determinadas circunstancias.
+        </li>
+        <li>El derecho de reportar una queja a una autoridad supervisora.</li>
+        <li>
+          El derecho a contactarnos en cualquier momento para retirar su
+          consentimiento proporcionado.
+        </li>
+      </ul>
+      Si desea ejercer sus derechos con respecto a los artículos mencionados
+      anteriormente con respecto a su información de identificación personal,
+      utilice la sección de contacto que se proporciona a continuación. Según lo
+      exige la ley, le responderemos en el momento apropiado.
+      <p />
+      <h4>Optar Por No y Retiro de Derechos</h4>
+      <p>
+        Utopicx respeta su privacidad y le brinda la opción durante cualquier
+        actividad de recopilación de información a negarse a proporcionar su
+        información de identificación personal.
+      </p>
+      <p>
+        La opción de suscribirse / aceptar nuevas alertas, contenido de
+        marketing y boletines informativos es suya. Puede optar por excluirse de
+        tales opciones en el enlace marcado como &quot;cancelar
+        suscripción&quot; en la parte inferior de los boletines informativos por
+        correo electrónico o las alertas que ha recibido de nosotros. Utopicx.
+        respetará sus deseos y cumplirá con sus preferencias de comunicación.
+      </p>
+      <h4>Responsabilidad de Autenticidad</h4>
+      <p>
+        Utopicx. no será responsable de la autenticidad de la información que
+        brinde y su deber es garantizar que la información que brinde sea
+        auténtica.
+      </p>
+      <h4>Protección de la Información de Menores</h4>
+      <p>
+        El sitio web de Utopicx no está dirigido, ni dirigido, a niños menores
+        de dieciséis (16) años de edad. Utopicx. no solicita, a sabiendas,
+        información de identificación personal ni comercializa sus servicios a
+        ninguna persona menor de dieciséis (16) años. Si tiene motivos para
+        creer que hemos recibido información de un menor, contáctenos tan pronto
+        como sea posible y haremos todos los esfuerzos razonables para eliminar
+        esa información de nuestra base de datos.
+      </p>
+      <h4>Enlaces a Sitios Web Externos / De Terceros</h4>
+      <p>
+        El sitio web de Utopicx puede tener enlaces a otros sitios web,
+        incluyendo, plataformas de redes sociales y sitios de terceros, cuyas
+        prácticas de recopilación de datos y recopilación de información son
+        diferentes a las nuestras. Utopicx no controla esas prácticas o sitios
+        de privacidad, y usted debe leer los avisos de privacidad asociados con
+        cualquier sitio web externo. Utopicx. no hace ninguna representación, ni
+        respaldamos, los sitios web de terceros.
+      </p>
+      <h4>Política de Retención y Almacenamiento de Datos</h4>
+      <p>
+        Su información de identificación personal se almacena en nuestras bases
+        de datos en servidores de servicios de administración de datos basados
+        en la nube con los que Utopicx se relaciona. Utopicx. mantendrá su PII
+        para fines comerciales y se mantendrá en línea con nuestras políticas de
+        retención de datos. Cualquier PII que se reciba de posibles / actuales
+        clientes se conservarán durante la duración de la relación comercial de
+        los posibles / actuales clientes con Utopicx.
+      </p>
+      <h4> Nuestro Continúo Compromiso Con Su Seguridad</h4>
+      <p>
+        Para salvaguardar la información que recopilamos, Utopicx. utiliza
+        procedimientos técnicos, administrativos y físicos apropiados. Para
+        garantizar que todos los asuntos relacionados con la información de
+        identificación personal se procesen de manera legal y justa, Utopicx
+        toma medidas razonables en sus transacciones de PII. Siempre que exista
+        un requisito comercial, o si así lo exigen otras leyes aplicables,
+        Utopicx puede conservar su PII.
+      </p>
+      <h4>Abordar las Quejas</h4>
+      <p>
+        Cualquier queja o reclamo relacionado con el procesamiento de la
+        información, debe enviarse por escrito a Utopicx. Por favor, consulte a
+        continuación la información de contacto.
+      </p>
+      <p>
+        Informacion de contacto:
+        <br />
+        Utopicx
+        <br />
+        Correo electrónico: contacto@utopicx.com
+        <br />
+        Dirección: Balcarce 1362
+        <br />
+        CABA,
+        <br />
+        Argentina
+      </p>
+      <h4>Cambios en la Política</h4>
+      <p>
+        Esta política puede cambiar. Por favor revise periódicamente si hay
+        actualizaciones o cambios en esta página. Esta página fue actualizada el
+        22-06-2023
+      </p>
+      <style jsx>{`
+        html,
+        body {
+          padding: 5px;
+          background-color: #ebe9e6;
+          min-width: 30rem;
+          max-width: 70%;
+        }
+        h4 {
+          font-size: 1.1rem;
+          font-weight: bold;
+          color: #242323;
+          margin-top: 0.5rem;
+          margin-bottom: 0.5rem;
+        }
+      `}</style>
+    </div>
+  );
+};
+
+export default Privacy;
